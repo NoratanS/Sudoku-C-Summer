daaf3d764ddc772531176142c9578bf5a6c2c22f
Krzysztof Bielski <kbielski23@gmail.com>
2025-04-28 17:56:00 +0200

    initial commit

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//#include "board.h"
+//#include "generator.h"
+//#include "ui.h"
+
+int main(void) {
+    printf("Hello, Sudoku!\n");
+    // TODO: initialize board, generator, UI
+    return 0;
+}
